536fabefa72ce37ebba01e89ff6b912f6d981ea8
Slava Petrov <slavik--petrov@yandex.ru>
2016-01-19 13:12:21 +0300

    Update test

--- hero_rspec.rb
+++ hero_rspec.rb
@@ -1,24 +1,24 @@
 require './hero'
 
 describe Hero do
-  it "has capitalized name" do
-    hero = Hero.new 'mike'
-    expect(hero.name).to eq 'Mike'
+  before do
+    @hero = Hero.new 'mike'  
+  end  
+
+  it "has capitalized name" do  
+    expect(@hero.name).to eq 'Mike'
   end
 
-  it "can power up" do
-    hero = Hero.new 'mike'
-    expect(hero.power_up).to eq 110
+  it "can power up" do    
+    expect(@hero.power_up).to eq 110
   end
 
-  it "can power down" do
-    hero = Hero.new 'mike'
-    expect(hero.power_down).to eq 90
+  it "can power down" do    
+    expect(@hero.power_down).to eq 90
   end
 
-  it "displays full hero info" do
-    hero = Hero.new 'mike', 25
-    expect(hero.hero_info).to eq "Mike has a health of 25"
+  it "displays full hero info" do    
+    expect(@hero.hero_info).to eq "Mike has a health of 100"
   end
 
 end
