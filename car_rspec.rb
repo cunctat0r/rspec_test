de49bd5dea60160ffe6d06ffa7647de88281622f
Slava Petrov <slavik--petrov@yandex.ru>
2016-01-19 14:07:44 +0300

    Add test for Car

--- car_rspec.rb
+++ car_rspec.rb
@@ -2,7 +2,14 @@ require './car'
 
 describe Car do 
 
-  it "" do
+  it "can drive for 200 miles" do
+    #arrange
+    car = Car.new
+    #act
+    car.add_fuel 10
+    #assert
+    expect(car.range).to eq 200
+
   end
   
 end
